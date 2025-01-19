a154644d7bd60de379b33fdc28a219d07726c9c5
daniel <daniel2501@gmail.com>
2025-01-19 09:54:39 -0500

    Added wpa_supplicant stuff

--- guix-setup.sh
+++ guix-setup.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 git clone https://github.com/daniel2501/etc.git
+mkdir /etc/wpa_supplicant
+cp wpa_supplicant.conf /mnt/etc/wpa_supplicant/
 cp etc/config.v3.scm /mnt/etc/
 cp etc/channels.scm /mnt/etc/
 guix time-machine -C /mnt/etc/channels.scm -- system init /mnt/etc/config.scm /mnt --substitute-urls='https://ci.guix.gnu.org https://bordeaux.guix.gnu.org https://substitutes.nonguix.org'
