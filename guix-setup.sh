69ef1b4b8c146ed4d119fe58d7e6b71ea89a2f23
daniel <daniel2501@gmail.com>
2025-01-19 10:01:37 -0500

    Corrected to be run from the repo dir.

--- guix-setup.sh
+++ guix-setup.sh
@@ -1,6 +1,6 @@
 mkdir /mnt/etc/wpa_supplicant
 cp wpa_supplicant.conf /mnt/etc/wpa_supplicant/
-cp etc/config.v3.scm /mnt/etc/
-cp etc/channels.scm /mnt/etc/
+cp config.v3.scm /mnt/etc/
+cp channels.scm /mnt/etc/
 guix time-machine -C /mnt/etc/channels.scm -- system init /mnt/etc/config.scm /mnt --substitute-urls='https://ci.guix.gnu.org https://bordeaux.guix.gnu.org https://substitutes.nonguix.org'
 
