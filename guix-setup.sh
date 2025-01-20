99db5016e9412f4d0d8b543ae1bada87e1a1af1d
daniel <daniel2501@gmail.com>
2025-01-20 14:10:47 -0500

    guix

--- guix-setup.sh
+++ guix-setup.sh
@@ -2,7 +2,7 @@ mkdir /mnt/etc/wpa_supplicant
 cp wpa_supplicant.conf /mnt/etc/wpa_supplicant/
 cp channels.scm /mnt/etc/
 cp /mnt/etc/config.scm config.installer.scm
-cp config.v3.scm config.edit.scm
+cp config.sway.scm config.edit.scm
 echo "Please edit ./config.edit.scm to have the disk uuids from ./config.installer.scm"
 # Open vim with both files
 vim -o config.edit.scm config.installer.scm
